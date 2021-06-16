3b630ed0a2191f5b344bdfa9ca0bf0ba7b6abf2b
rajaprasad paikaray <43632842+hyraja@users.noreply.github.com>
2021-06-16 19:53:01 +0530

    reverse of a string

---
+++ Interview_python/reverse_str_lst.py
@@ -0,0 +1,28 @@
+# findout reverse of given string
+
+# method 1(Using loop)
+# def reverse(s):
+#     str = ''
+#     for i in s:
+#         str = i + str
+#     return str
+
+
+# print(reverse('rajaprasad')) # dasarpajar
+
+# method 2(using indexing)
+# def reverse(s):
+#     p = s[::-1]
+#     return p
+
+
+# print(reverse('rajaprasad')) # dasarpajar
+
+# method 3 (using reversed)
+# def reverse(string):
+#     string = ''.join(reversed(string))
+#     return string
+
+#   # dasarpajar
+# s = 'rajaprasad'
+# print(reverse(s))
