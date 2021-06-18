224e4e064fa8dab5f0b39ee41373ba6b9332da5a
rajaprasad paikaray <43632842+hyraja@users.noreply.github.com>
2021-06-18 11:47:19 +0530

    Update fibbonanci.py

--- Interview_python/fibbonanci.py
+++ Interview_python/fibbonanci.py
@@ -1,26 +1,25 @@
 # find out fibbonanci of given number
 
 # method 1 (using generator)
-def fib(num):
-    a, b = 0, 1
-    for i in range(num):
-        #print('i value', i)
-        yield a
-        a, b = b, a+b
+# def fib(num):
+#     a, b = 0, 1
+#     for i in range(num):
+#         #print('i value', i)
+#         yield a
+#         a, b = b, a+b
 
 
-for x in fib(5):
-    print(f' fibbonaci {x}')
+# for x in fib(5):
+#     print(f' fibbonaci {x}')
 
 
 # method 2 (using while)
 
-# def fib(n):
-#     a, b = 0, 1
-#     while a < n:
-#         print(a, end=' ')
-#         a, b = b, a+b
-#     print()
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
 
 
-# fib(5)
+fib(5)
