6cf34dc0de85940126efefda9c362ed524b1b9eb
rajaprasad paikaray <43632842+hyraja@users.noreply.github.com>
2021-06-26 11:13:21 +0530

    Create pattern_print.py

---
+++ Interview_python/pattern_print.py
@@ -0,0 +1,8 @@
+# I have an input 'Hello' i can get the o/p in this way 'H He Hel Hell Hello'
+
+# st = input("enter a string : ")
+# for i in range(len(st)):
+#     print(st[:i+1], end=" ")
+
+# enter a string : Hello
+# H He Hel Hell Hello
