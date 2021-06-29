3c8befc3293bd2fe2a2f68954d4d95783c770bc0
rajaprasad paikaray <43632842+hyraja@users.noreply.github.com>
2021-06-29 14:42:34 +0530

    Update not_repeat_element.py

--- Interview_python/not_repeat_element.py
+++ Interview_python/not_repeat_element.py
@@ -3,10 +3,13 @@
 li = [1, 3, 1, 3, 4, 5, 6, 7, 8, 1, 5, 3]
 
 elst = []
-for i in li:
-    if li.count(i) == 1:
-        #        if i not in elst:
-        elst.append(i)
-print(elst)
+# for i in li:
+#     if li.count(i) == 1:
+#         #        if i not in elst:
+#         elst.append(i)
 
-#[4, 6, 7, 8]
+[elst.append(i) for i in li if li.count(i) == 1]
+[elst.append(i) for i in li if li.count(i) > 1 if i not in elst]  # [1,3,5]
+
+print(elst)  # [4, 6, 7, 8]
+# print(x) # [None, None, None, None]
